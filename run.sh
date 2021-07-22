b52029f6f6786b4c04dae4ff0599ace8f9f75a69
Jonas Loehr <jonas.loehr@hm.edu>
2021-07-22 11:16:51 +0200

    set IP address for our robot

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set echo off
-read IP=10.96.210.44
+read IP=10.96.210.56
 title "Run LegoRobo"
 echo "Running..."
 ssh -oKexAlgorithms=+diffie-hellman-group1-sha1 -c 3des-cbc root@%IP% "jrun -jar /home/lejos/programs/LegoRoboter.jar"
--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set echo off
-read IP=10.96.210.44
+read IP=10.96.210.56
 title "Upload LegoRobo"
 echo "Uploading..."
 pscp -P 22 -scp out/LegoRoboter.jar root@%IP%:/home/lejos/programs
