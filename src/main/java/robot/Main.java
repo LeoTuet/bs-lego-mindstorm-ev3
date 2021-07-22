6b8d6e0ecd21b1e3356b605cc9eb032865ac1628
Jonas Loehr <jonas.loehr@hm.edu>
2021-07-22 11:35:51 +0200

    implemented reverseParking routine

--- src/main/java/robot/Main.java
+++ src/main/java/robot/Main.java
@@ -5,7 +5,15 @@ import robot.api.TwoWheelRobot;
 public class Main {
 
     public static void main(String[] args) {
+        reverseParking();
+    }
+
+    private static void reverseParking() {
         TwoWheelRobot wheelRobot = new TwoWheelRobot();
-        wheelRobot.drive(5);
+        wheelRobot.setSpeed(1);
+        wheelRobot.driveDist(80);
+        wheelRobot.turn('l', 90);
+        wheelRobot.setSpeed(-1);
+        wheelRobot.driveDist(40);
     }
 }
