032db625e9348b6e05aa9d144e7b09fabb6227b5
Jonas Loehr <jonas.loehr@hm.edu>
2021-07-22 11:23:50 +0200

    updated shell scripts

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 set echo off
-read IP=10.96.210.56
-title "Run LegoRobo"
 echo "Running..."
-ssh -oKexAlgorithms=+diffie-hellman-group1-sha1 -c 3des-cbc root@%IP% "jrun -jar /home/lejos/programs/LegoRoboter.jar"
-pause
+ssh -oKexAlgorithms=+diffie-hellman-group1-sha1 -c 3des-cbc root@10.96.210.56 "jrun -jar /home/lejos/programs/LegoRoboter.jar"
+
--- upload.sh
+++ upload.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 set echo off
-read IP=10.96.210.56
-title "Upload LegoRobo"
 echo "Uploading..."
-pscp -P 22 -scp out/LegoRoboter.jar root@%IP%:/home/lejos/programs
-pause
+pscp -P 22 -scp out/LegoRoboter.jar root@10.96.210.56:/home/lejos/programs
