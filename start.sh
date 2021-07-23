4ed4b40b1e6e946d074c9192bf3c16d12b8438ce
Marc Schwaiger <marcschwaiger@outlook.de>
2021-07-23 10:33:36 +0200

    Added error messages to start.sh and made it read the ip.txt

--- start.sh
+++ start.sh
@@ -1,7 +1,23 @@
-IP=10.96.210.58
+IP=$(cat ip.txt)
 JAR=LegoRoboter-1.0-SNAPSHOT.jar
-printf "Uploading...\n"
+
+printf "Uploading to %s...\n" "$IP"
 scp -oKexAlgorithms=+diffie-hellman-group1-sha1 -c 3des-cbc target/$JAR root@$IP:/home/lejos/programs
-printf "Running...\n"
+
+if [ $? -gt 0 ]
+then
+  echo -e "\e[31mUpload failed\e[0m"
+  exit 1
+fi
+
+printf "Running %s...\n" "$JAR"
 ssh -oKexAlgorithms=+diffie-hellman-group1-sha1 -c 3des-cbc root@$IP "jrun -jar /home/lejos/programs/$JAR"
+
+if [ $? -gt 0 ]
+then
+  echo -e "\e[31mRun failed\e[0m"
+  exit 1
+fi
+
 printf "Done!\n"
+exit 0
