3148bbd206b0a3b818b85a48d8db1d8599a20b64
Leo <leopold@tueting.net>
2021-07-27 08:21:32 +0200

    added tutorial images

--- src/main/java/robot/Main.java
+++ src/main/java/robot/Main.java
@@ -2,6 +2,7 @@ package robot;
 
 import robot.api.TwoWheelRobot;
 
+
 public class Main {
 
     public static void main(String[] args) {
--- src/main/java/robot/api/Helper.java
+++ src/main/java/robot/api/Helper.java
@@ -4,13 +4,17 @@ import lejos.utility.Delay;
 
 
 public class Helper {
-	public static final double DIAMETER = 5.6;
-	public static final double TRACKWIDTH = 11.7;
-	private Helper(){}
-	public static void delayProgramm(double sek) {
-		Delay.msDelay((int)(sek*1000));
-	}
-	public static double getRotations(double cm) {
-		return cm/(DIAMETER *Math.PI);
-	}
+    public static final double DIAMETER = 5.6;
+    public static final double TRACKWIDTH = 11.7;
+
+    private Helper() {
+    }
+
+    public static void delayProgramm(double sek) {
+        Delay.msDelay((int) (sek * 1000));
+    }
+
+    public static double getRotations(double cm) {
+        return cm / (DIAMETER * Math.PI);
+    }
 }
